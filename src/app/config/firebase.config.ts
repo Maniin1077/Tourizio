5e416b736b74a63374a8d62ac8cb8bdeb2fb0e1e
Baratam Sri Manikanta <bsmanikanta2004@gmail.com>
2025-10-23 12:41:03 +0530

    Update firebase.config.ts

--- src/app/config/firebase.config.ts
+++ src/app/config/firebase.config.ts
@@ -2,8 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { environment } from '../../environments/environment';
 
-// Initialize Firebase app using environment config
 const app = initializeApp(environment.firebaseConfig);
 
-// Export auth instance for use across the app
 export const auth = getAuth(app);
